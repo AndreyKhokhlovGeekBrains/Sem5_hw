5460cb80a2bfe11aaadaf95f792c5e1a1e9cbcec
Andrey Khokhlov <kron-k@ya.ru>
2024-06-13 18:57:12 +0300

    Added changes to task_5

--- task_5.c
+++ task_5.c
@@ -13,14 +13,11 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
 
 char* check_brackets(const char* my_string)
 {
     int length = strlen(my_string);
-    if(length > 1000)
-    {
-        return "The length of the string is greater than 1000 characters. Please enter a string of less than 1000 characters.";
-    }
 
     int check_count = 0;
     for(int i = 0; i < length; i++)
@@ -44,12 +41,28 @@ char* check_brackets(const char* my_string)
 
 int main(void)
 {
-    printf("%s\n", check_brackets("() )("));  // Should output NO
-    printf("%s\n", check_brackets("(())"));   // Should output YES
-    printf("%s\n", check_brackets(")("));     // Should output NO
-    printf("%s\n", check_brackets("()()"));   // Should output YES
-    printf("%s\n", check_brackets("()(."));     // Should output NO
-    printf("%s\n", check_brackets("(()())."));   // Should output YES
+    char my_string[1001];  // Allocate a buffer of 1001 characters (1000 + 1 for the null terminator)
+    int i = 0;
+    char c;
+
+    printf("Enter a string sequence which ends by a dot (.)\n");
+    while ((c = getche()) != '.')
+    {
+        if(i < 1000) 
+        {
+            my_string[i++] = c;
+        }
+        else
+        {
+            printf("\nThe length of the string is greater than 1000 characters which is not allowed.");
+            break;
+        }
+    }
+    my_string[i] = '\0';  // Null-terminate the string
+    printf("\n");
+
+    printf("The accepted string is:\n%s\n", my_string);
+    printf("%s\n", check_brackets(my_string));
 
     return 0;
 }
