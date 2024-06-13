6b5b37a0a0b3949806c40d365b203dcc784681b8
Andrey Khokhlov <kron-k@ya.ru>
2024-06-13 19:47:17 +0300

    Refactored my code

--- task_5.c
+++ task_5.c
@@ -12,57 +12,42 @@
 // Данные на выходе: NO
 
 #include <stdio.h>
-#include <string.h>
 #include <conio.h>
 
-char* check_brackets(const char* my_string)
+char* check_brackets()
 {
-    int length = strlen(my_string);
-
     int check_count = 0;
-    for(int i = 0; i < length; i++)
-    {
-        char ch = my_string[i];
-        if(check_count < 0)
-        {
-            return "NO";
-        }
-        if(ch == '(')
-        {
-            check_count ++;
-        } else if (ch == ')')
-        {
-            check_count --;
-        } 
-    }
-    return (check_count == 0) ? "YES" : "NO";
-}
-
-
-int main(void)
-{
-    char my_string[1001];  // Allocate a buffer of 1001 characters (1000 + 1 for the null terminator)
     int i = 0;
     char c;
 
     printf("Enter a string sequence which ends by a dot (.)\n");
     while ((c = getche()) != '.')
     {
-        if(i < 1000) 
+        if(i < 3) 
         {
-            my_string[i++] = c;
+            if (c == '(') 
+            {
+                check_count++;
+            }
+            else if (c == ')')
+            {
+                check_count--;
+            }
+            i++;
         }
         else
         {
-            printf("\nThe length of the string is greater than 1000 characters which is not allowed.");
+            printf("\nThe length of the string is greater than 1000 characters which is not allowed.\n");
             break;
         }
     }
-    my_string[i] = '\0';  // Null-terminate the string
-    printf("\n");
+    return (check_count == 0) ? "\nYES" : "\nNO";
+}
 
-    printf("The accepted string is:\n%s\n", my_string);
-    printf("%s\n", check_brackets(my_string));
+
+int main(void)
+{
+    printf("%s\n", check_brackets());
 
     return 0;
 }
